a0e400ba4d9c83d510f3abaa1b7c96cb74c05c4d
Everton Dutra <evertondutra02@gmail.com>
2021-08-04 13:41:37 -0300

    colocando por paginas

--- src/main/java/com/evertondutra/citiesapi/CountryResource.java
+++ src/main/java/com/evertondutra/citiesapi/CountryResource.java
@@ -2,19 +2,30 @@ package com.evertondutra.citiesapi;
 
 import com.evertondutra.citiesapi.countries.Country;
 import com.evertondutra.citiesapi.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/countries")
 public class CountryResource {
 
+
     private CountryRepository repository;
 
+    public CountryResource(CountryRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
     }
 }
