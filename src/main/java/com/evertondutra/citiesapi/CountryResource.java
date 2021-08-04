1f663a48f75ffcffd2143fcdfc5af2e0e72361f1
Everton Dutra <evertondutra02@gmail.com>
2021-08-04 14:10:01 -0300

    Mapeando por id

--- src/main/java/com/evertondutra/citiesapi/CountryResource.java
+++ src/main/java/com/evertondutra/citiesapi/CountryResource.java
@@ -2,15 +2,14 @@ package com.evertondutra.citiesapi;
 
 import com.evertondutra.citiesapi.countries.Country;
 import com.evertondutra.citiesapi.repository.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -27,5 +26,11 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public Country getOne(@PathVariable Long id) {
+        Optional<Country>optional = repository.findById(id);
+        return optional.get();
     }
 }
